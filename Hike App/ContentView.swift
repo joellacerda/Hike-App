aea7f56a01df6d38f6ad02ccbc342b82350f8986
Joel Lacerda <joellacerdaol@gmail.com>
2024-03-13 10:21:45 -0300

    Initial Commit

---
+++ Hike App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike App
+//
+//  Created by Joel Lacerda on 13/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike App/Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_AppApp.swift
+//  Hike App
+//
+//  Created by Joel Lacerda on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
