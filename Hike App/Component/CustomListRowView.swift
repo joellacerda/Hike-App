3a70265634c752e938047bfe23c147fd6a94fce2
Joel Lacerda <joellacerdaol@gmail.com>
2024-04-01 17:29:18 -0300

    add settings list and labeled content

---
+++ Hike App/Component/CustomListRowView.swift
@@ -0,0 +1,59 @@
+//
+//  CustomListRowView.swift
+//  Hike App
+//
+//  Created by Joel Lacerda on 01/04/24.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    // MARK: - PROPERTIES
+    @State var rowLabel: String
+    @State var rowIcon: String
+    @State var rowContent: String? = nil
+    @State var rowTintColor: Color
+    @State var rowLinkLabel: String? = nil
+    @State var rowLinkDestination: String? = nil
+    
+    var body: some View {
+        LabeledContent {
+            if rowContent != nil {
+                Text(rowContent!)
+                    .foregroundStyle(.primary)
+                    .fontWeight(.heavy)
+            } else if (rowLinkLabel != nil && rowLinkDestination != nil) {
+                Link(rowLinkLabel!, destination: URL(string: rowLinkDestination!)!)
+                    .foregroundStyle(.pink)
+                    .fontWeight(.heavy)
+            } else {
+                EmptyView()
+            }
+        } label: {
+            HStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(rowTintColor)
+                    Image(systemName: rowIcon)
+                        .foregroundStyle(.white)
+                        .fontWeight(.semibold)
+                }
+                Text(rowLabel)
+            }
+        }
+    }
+}
+
+#Preview {
+    List {
+        CustomListRowView(
+            rowLabel: "Website",
+            rowIcon: "globe",
+            rowContent: nil,
+            rowTintColor: .pink,
+            rowLinkLabel: "Credo Academy",
+            rowLinkDestination: "https://credo.academy"
+        )
+    }
+}
--- Hike App/Screen/SettingsView.swift
+++ Hike App/Screen/SettingsView.swift
@@ -55,6 +55,27 @@ struct SettingsView: View {
             // MARK: - SECTION: ICONS
             
             // MARK: - SECTION: ABOUT
+            Section(
+                header: Text("ABOUT THE APP"),
+                footer: HStack {
+                    Spacer()
+                    Text("Copyright © All rights reserved.")
+                    Spacer()
+                }
+                    .padding(.vertical, 8)
+            ) {
+                // 1. Basic Labeled Content
+                // LabeledContent("Application", value: "Hike")
+                
+                //2. Advanced Labeled Content
+                CustomListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "Hike", rowTintColor: .blue)
+                CustomListRowView(rowLabel: "Compatibility", rowIcon: "info.circle", rowContent: "iOS, iPadOS", rowTintColor: .red)
+                CustomListRowView(rowLabel: "Technology", rowIcon: "swift", rowContent: "Swift", rowTintColor: .orange)
+                CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
+                CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "Joel Lacerda", rowTintColor: .mint)
+                CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petrus", rowTintColor: .pink)
+                CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Academy", rowLinkDestination: "https://credo.academy")
+            }
         }
     }
 }
